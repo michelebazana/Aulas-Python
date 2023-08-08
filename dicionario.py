ffa31f536676a9bcf56db1480307542687e2c302
michelebazana <michele.bazana@gmail.com>
2023-08-07 22:31:00 -0300

    Aula sobre dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,1 +1,22 @@
 #Aula sobre dicionário
+
+carros ={"Jeep Renegade":['R$90.000,00','2018'],
+         "Jeep Compass": 'R$150.000,00',
+         "Troller": 'R$200.000,00'}
+print(carros)
+print(carros["Jeep Renegade"])
+
+carros["Jeep Compass"] = 'R$180.000,00'
+print(carros)
+
+carros["Jeep Renegade"][1] = "2016"
+print(carros)
+
+del carros["Troller"]
+print(carros)
+
+carros["Audi"] = ['R$250.000,00', "2023"]
+print(carros)
+
+print("Audi" in carros)
+print("BMW" in carros)
