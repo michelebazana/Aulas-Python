0d77ab684572467c73eb48e9a1b5e2438e4a5837
michelebazana <michele.bazana@gmail.com>
2024-03-18 21:04:07 -0300

    Minha terceira aula em python.

--- terceira_aula.py
+++ terceira_aula.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+# media_final = int(input("Média final = "))
+# if media_final >= 70:
+#     print("Aprovado!")
+# else:
+#     print("Reprovado!")
+
+#exercício 1
+vel = int(input("Entre com a velocidade do carro em km/h = "))
+if vel > 80:
+    print("Você foi multado!")
+    multa = (vel - 80) * 5
+    print(f"O valor da sua multa é R$ {multa:.2f}.")
